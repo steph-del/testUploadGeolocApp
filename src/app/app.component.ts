e590d4aabfebf0eee350a8a6f0656af54b876dd3
steph-del <dev.stephane.delplanque@gmail.com>
2018-12-19 20:26:54 +0100

    liaison entre les deux modules pour la geolocalisation des photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
+  geolocatedPhotos = [];
+
+  geolocatedPhotoData(data) {
+    this.geolocatedPhotos = data;
+  }
 }
